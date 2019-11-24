5f9f31f308fe9f241c4f52695ef845e90939ae29
potatoTVnet <js@potatotv.net>
2019-11-24 23:51:55 +0100

    added sending capabilities

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # [EXPERIMENTAL] RemoteServer
  
 currently only relaying tcp packages on the console, might be used for data analysis, game state sharing or other second-screen experiences later...
+
+start program by running ``npm start``
--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 const Net = require('net')
-const port = 12000
+const port = 12001
 
 const server = new Net.Server()
 
-server.listen(port, function() {
+server.listen(port, function () {
     console.log(`Server listening for connection requests on socket localhost:${port}`)
 })
 
-server.on('connection', function(socket) {
-    console.log('A new connection has been established.')
+server.on('connection', function (socket) {
+    console.log('CONNECTED')
 
-    socket.on('data', function(chunk) {
-        console.log(`Data received from client: ${chunk.toString()}`)
+    socket.on('data', function (chunk) {
+        console.log(`${chunk.toString()}`)
     })
 
-    socket.on('end', function() {
-        console.log('Closing connection with the client')
+    socket.on('end', function () {
+        console.log('CONNECTION CLOSED')
     })
 
-    socket.on('error', function(err) {
-        console.log(`Error: ${err}`)
+    socket.on('error', function (err) {
+        //console.log(`Error: ${err}`)
     })
+
+    readline.on('line', function (input) {
+        if(input == "Q") process.exit(1)
+        socket.write(input)
+    })
+    
 })
+
